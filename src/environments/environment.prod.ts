2b59699adbe62e6b0d3f4a03462a67f5a93d58ea
Ribal <ribal.nasr@gmail.com>
2020-02-22 11:34:32 +0200

    Code cleanup.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyAxvw2MEcMAx_Cuq4ky_lCaycGOKbVI2rg',
+    authDomain: 'alwaleed-philanthropies-site.firebaseapp.com',
+    databaseURL: 'https://alwaleed-philanthropies-site.firebaseio.com',
+    projectId: 'alwaleed-philanthropies-site',
+    storageBucket: 'alwaleed-philanthropies-site.appspot.com',
+    messagingSenderId: '349067577808',
+    appId: '1:349067577808:web:3a00ae6eda9d1ed8'
   }
 
 };
